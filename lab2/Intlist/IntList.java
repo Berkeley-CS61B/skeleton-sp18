27bcfbecff9f2434e097745ed0327d17b3a76cee
Po An Chou <you@example.com>
2019-01-19 10:43:57 +0800

    complete lab2 v1.1

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -82,7 +82,7 @@ public class IntList {
 
     public static IntList dcatenate(IntList A, IntList B) {
         if(A == null){
-            return null;
+            return B;
         }
         IntList res = A;
         res = res.rest;
