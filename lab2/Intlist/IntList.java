ad7f4c1838fefce95148144e607bcd6a5aa29acb
Adnan Mohamed <adnan.hashem.bh@gmail.com>
2020-08-21 18:31:58 +0300

    Fixing dcatenate.
    More info:-
    The function dcatenate did not handle the case when B
    is null. So the following commit is fixing this issue.

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -81,6 +81,8 @@ public class IntList {
      */
 
     public static IntList dcatenate(IntList A, IntList B) {
+        if(B == null)
+            return A;
         IntList cursor = A;
 
         // move the cursor to the tail of A.
