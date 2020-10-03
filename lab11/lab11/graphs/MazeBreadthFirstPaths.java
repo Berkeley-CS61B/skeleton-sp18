45879aa3448dd6c38d3c1cbe30f48492c84bbe3e
Adnan Mohamed <adnan.hashem.bh@gmail.com>
2020-10-03 08:22:30 +0300

    The code in Solver works (but no optimization)

--- lab11/lab11/graphs/MazeBreadthFirstPaths.java
+++ lab11/lab11/graphs/MazeBreadthFirstPaths.java
@@ -26,7 +26,6 @@ public class MazeBreadthFirstPaths extends MazeExplorer {
 
     /** Conducts a breadth first search of the maze starting at the source. */
     private void bfs() {
-        // TODO: Your code here. Don't forget to update distTo, edgeTo, and marked, as well as call announce()
         marked[s] = true;
         announce();
         Queue<Integer> q = new ArrayDeque<>();
--- lab11/lab11/graphs/MazeCycles.java
+++ lab11/lab11/graphs/MazeCycles.java
@@ -1,5 +1,7 @@
 package lab11.graphs;
 
+import java.util.Random;
+
 /**
  *  @author Josh Hug
  */
@@ -9,16 +11,37 @@ public class MazeCycles extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
-
+    private int[] cameFrom;
+    private boolean hasCycle = false;
     public MazeCycles(Maze m) {
         super(m);
     }
 
     @Override
     public void solve() {
-        // TODO: Your code here!
+        cameFrom = new int[maze.V()];
+        int xStart = new Random().nextInt(maze.N()) + 1;
+        int yStart = new Random().nextInt(maze.N()) + 1;
+        int s = maze.xyTo1D(xStart, yStart);
+
+        cameFrom[s] = s;
+        dfs(maze, s);
+        announce();
     }
 
     // Helper methods go here
+    public void dfs(Maze m, int v) {
+        marked[v] = true;
+        for (int node : m.adj(v)) {
+            if (!marked[node]) {
+                //edgeTo[node] = v;
+                cameFrom[node] = v;
+                dfs(m, node);
+            } else if (cameFrom[node] != v) {
+                hasCycle = true;
+                return;
+            }
+        }
+    }
 }
 
